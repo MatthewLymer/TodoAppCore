bce9caf5f0e46381ad2568b9a6ca95e9f453e7b0
Matthew Lymer <matthew@lymer.ca>
2016-11-20 20:01:05 -0500

    (try to) add Karma test runner

---
+++ TodoApp.Web/app/greeter.spec.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="../node_modules/@types/jasmine/index.d.ts"/>
+
+import {Greeter} from "./Greeter";
+
+describe("Greeter", () => {
+    it('should greet', () => {
+        var greeter = new Greeter();
+
+        var message = greeter.greet("Matthew");
+
+        expect(message).toBe("Hello Matthew");
+    });
+});
---
+++ TodoApp.Web/wwwroot/config.js
@@ -0,0 +1,8 @@
+/// <reference path="./jspm_packages/system.js"/>
+
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: false,
+  paths: {}
+});
