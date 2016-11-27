6d3c68c9194e2d65bf8c1f755596bcbdc2983881
Matthew Lymer <matthew.lymer@outlook.com>
2016-11-27 16:38:11 -0500

    include angular2 jspm package

--- TodoApp.Web/app/main.ts
+++ TodoApp.Web/app/main.ts
@@ -1,5 +1,20 @@
-﻿import {Greeter} from "./Greeter";
+﻿/// <reference path="../node_modules/angular2/core.d.ts"/>
+/// <reference path="../node_modules/angular2/typings/es6-promise/es6-promise.d.ts"/>
+/// <reference path="../node_modules/angular2/typings/browser.d.ts"/>
+/// <reference path="../node_modules/@types/rx/rx.all.d.ts"/>
 
-const greeter = new Greeter();
-const element = document.getElementById("greeting");
-element.innerHTML = greeter.greet("Matthew"); 
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'hello-world',
+    template: `<h1>Hello {{ name }}!</h1>`
+})
+class HelloWorldComponent {
+    name: string;
+
+    constructor() {
+        this.name = 'Angular 2';
+    }
+}
+
+bootstrap(HelloWorldComponent);
